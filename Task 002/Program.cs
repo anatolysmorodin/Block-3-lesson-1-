1dd899b0a5fa7589df54fc77360aa9ad46eda16a
Anatoly <a.smorodin@bk.ru>
2022-12-16 18:12:47 +0300

    Task 002 failure

---
+++ Task 002/Program.cs
@@ -0,0 +1,16 @@
+﻿// по заданному номеру дня недели вывести его название //
+
+int a = Monday;
+int b = Tuesday;
+int c = Wednesday;
+int d = Thursday;
+int e = Friday;
+int f =  Saturday;
+int g = Sunday;
+
+int a = min;
+int g = max;
+
+new Random().Next(min,max);
+Console.WriteLine(result);
+
