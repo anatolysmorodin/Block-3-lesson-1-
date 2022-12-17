8b4a622ab847f479d54575f52338c6b568e891c2
Anatoly <a.smorodin@bk.ru>
2022-12-17 14:04:14 +0300

    Task 006 complete

---
+++ Task 006/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число");
+int A = int.Parse(Console.ReadLine()!);
+int B = A / 10 % 10;
+Console.WriteLine(B);
+
