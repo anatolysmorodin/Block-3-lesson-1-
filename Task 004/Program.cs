faf5dc11faa1c7e3406ab4f243f004d4fdf2549e
Anatoly <a.smorodin@bk.ru>
2022-12-17 13:04:38 +0300

    Task 004 false

---
+++ Task 004/Program.cs
@@ -0,0 +1,11 @@
+﻿//Выяснить является ли число чётным
+Console.Write("Введите целое число");
+int A = (Console.ReadLine);
+if (A % 2) 
+{
+    Console.WriteLine ("Число четное");
+}
+else 
+{
+    Console.WriteLine ("Число не четное");
+}
