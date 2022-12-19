4a2ad82cad4137afac472bbe64191319a20a82ce
Anatoly <a.smorodin@bk.ru>
2022-12-20 00:57:27 +0300

    Files returned

--- Task 001/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// По двум заданным числам проверять, является ли первое квадратом второго //
-int A = 49;
-int B = 7;
-if (B * B == A) {
-    Console.WriteLine(true);
-}
-else Console.WriteLine (false);
--- Task 006/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// Показать вторую цифру трёхзначного числа
-
-Console.WriteLine("Введите трехзначное число");
-int A = int.Parse(Console.ReadLine()!);
-int B = A / 10 % 10;
-Console.WriteLine(B);
-
