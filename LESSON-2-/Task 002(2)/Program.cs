039c742bdf66d20f50423cf079400d156e82a0eb
Anatoly <a.smorodin@bk.ru>
2022-12-19 15:36:57 +0300

    Task  002 complete

---
+++ LESSON-2-/Task 002(2)/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine()!);
+int B = A / 100 % 10; 
+if (B % 10 > 0) 
+{
+    Console.Write("Третья цифра ");
+    Console.WriteLine(B);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
