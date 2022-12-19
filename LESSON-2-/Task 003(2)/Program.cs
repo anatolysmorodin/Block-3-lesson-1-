f649c715280e1cfd47f855cf0eedf8e9dceb9db0
Anatoly <a.smorodin@bk.ru>
2022-12-19 16:14:55 +0300

    Task 003(2) complete

---
+++ LESSON-2-/Task 003(2)/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, //
+// и проверяет, является ли этот день выходным.  //
+
+Console.WriteLine("введите число от 1 до 7");
+int day = int.Parse (Console.ReadLine()!);
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+
+if (day == a) {Console.WriteLine("Monday - working day");}
+if (day == b) {Console.WriteLine("Tuesday - working day");}
+if (day == c) {Console.WriteLine("Wednesday - working day");}
+if (day == d) {Console.WriteLine("Thursday - working day");}
+if (day == e) {Console.WriteLine("Friday - working day");}
+if (day == f) {Console.WriteLine("Saturday - weekend");}
+if (day == g) {Console.WriteLine("Sunday - weekend");}
+
+
